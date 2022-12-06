460cc2e9a44122583fd82f113b8aab61a54f2fa5
Fikri Arkani Salim <fikriarkani02@gmail.com>
2022-12-06 21:06:08 +0700

    Perbaikan Vigenere untul key.length > str.length

--- Vigenere/vigenere.js
+++ Vigenere/vigenere.js
@@ -2,48 +2,93 @@ function cipherText(){
     var str = document.getElementById("text").value;
     var keyless = document.getElementById("key").value;
 
-    var key = generatekey(str,keyless);
-
+    var lengthStr = parseInt(str.length);
+    var lengthKey = parseInt(keyless.length);
     var ciphertext = ""; 
 
     var conkey = 0,tempkey, tempstr, x;
 
-    for(let i = 0; i < str.length ; i++){
-        if(str[i] == ' '){
-            ciphertext += ' ';
-            conkey += 1;
-            continue;
+    if(lengthKey <= lengthStr){
+        var key = generatekey(str,keyless);
+        for(let i = 0; i < str.length ; i++){
+            if(str[i] == ' '){
+                ciphertext += ' ';
+                conkey += 1;
+                continue;
+            }
+        
+
+            if(str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90){
+                tempstr = str.charCodeAt(i) - 65;
+            }
+
+            if(str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122){
+                tempstr = str.charCodeAt(i) - 97;
+            }
+
+            if(key.charCodeAt(i-conkey) >= 65 && key.charCodeAt(i) <= 90){
+                tempkey = key.charCodeAt(i-conkey) - 65;
+            }
+
+            if(key.charCodeAt(i-conkey) >= 97 && key.charCodeAt(i) <= 122){
+                tempkey = key.charCodeAt(i-conkey) - 97;
+            }
+
+            x = (tempstr + tempkey) % 26;
+
+            if(str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90){
+                x += 65;
+            }
+
+            if(str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122){
+                x += 97;
+            }
+            ciphertext += String.fromCharCode(x);
         }
+        document.getElementById("hasil").innerHTML = ciphertext; 
+    } else {
+        var key ="";
+        for(let i = 0;i<str.length; i++){
+            key += keyless[i];
+        }
+        
+        for(let i = 0; i < str.length ; i++){
+            if(str[i] == ' '){
+                ciphertext += ' ';
+                conkey += 1;
+                continue;
+            }
+        
     
-
-        if(str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90){
-            tempstr = str.charCodeAt(i) - 65;
+            if(str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90){
+                tempstr = str.charCodeAt(i) - 65;
+            }
+    
+            if(str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122){
+                tempstr = str.charCodeAt(i) - 97;
+            }
+    
+            if(key.charCodeAt(i-conkey) >= 65 && key.charCodeAt(i) <= 90){
+                tempkey = key.charCodeAt(i-conkey) - 65;
+            }
+    
+            if(key.charCodeAt(i-conkey) >= 97 && key.charCodeAt(i) <= 122){
+                tempkey = key.charCodeAt(i-conkey) - 97;
+            }
+    
+            x = (tempstr + tempkey) % 26;
+    
+            if(str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90){
+                x += 65;
+            }
+    
+            if(str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122){
+                x += 97;
+            }
+            ciphertext += String.fromCharCode(x);
         }
-
-        if(str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122){
-            tempstr = str.charCodeAt(i) - 97;
-        }
-
-        if(key.charCodeAt(i-conkey) >= 65 && key.charCodeAt(i) <= 90){
-            tempkey = key.charCodeAt(i-conkey) - 65;
-        }
-
-        if(key.charCodeAt(i-conkey) >= 97 && key.charCodeAt(i) <= 122){
-            tempkey = key.charCodeAt(i-conkey) - 97;
-        }
-
-        x = (tempstr + tempkey) % 26;
-
-        if(str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90){
-            x += 65;
-        }
-
-        if(str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122){
-            x += 97;
-        }
-        ciphertext += String.fromCharCode(x);
+        document.getElementById("hasil").innerHTML = ciphertext;
     }
-    document.getElementById("hasil").innerHTML = ciphertext;
     
 }
 
@@ -51,50 +96,102 @@ function decrypt(){
     var ciphertext = document.getElementById("text").value;
     var keyLess = document.getElementById("key").value;
 
-    var key = generatekey(ciphertext,keyLess);
 
     var plaintext="";
 
     var conkey = 0, tempkey, tempciphertext, x;
 
-    for(let i = 0 ; i < ciphertext.length ; i++ ){
-        if(ciphertext[i] == ' '){
-            plaintext += ' ';
-            conkey += 1;
-            continue;
+
+
+    var lengthCipher = parseInt(ciphertext.length);
+    var lengthKey = parseInt(keyLess.length);
+
+    if(lengthKey <= lengthCipher){
+        var key = generatekey(ciphertext,keyLess);
+        for(let i = 0 ; i < ciphertext.length ; i++ ){
+            if(ciphertext[i] == ' '){
+                plaintext += ' ';
+                conkey += 1;
+                continue;
+            }
+
+            if(ciphertext.charCodeAt(i) >= 65 && ciphertext.charCodeAt(i) <= 90){
+                tempciphertext = ciphertext.charCodeAt(i) - 65;
+            }
+
+            if(ciphertext.charCodeAt(i) >= 97 && ciphertext.charCodeAt(i) <= 122){
+                tempciphertext = ciphertext.charCodeAt(i) - 97;
+            }
+
+            if(key.charCodeAt(i-conkey) >= 65 && key.charCodeAt(i) <= 90){
+                tempkey = key.charCodeAt(i-conkey) - 65;
+            }
+
+            if(key.charCodeAt(i-conkey) >= 97 && key.charCodeAt(i) <= 122){
+                tempkey = key.charCodeAt(i-conkey) - 97;
+            }
+
+            x = ma_mod((tempciphertext - tempkey),26);
+
+            if(ciphertext.charCodeAt(i) >= 65 && ciphertext.charCodeAt(i) <= 90){
+                x += 65;
+            }
+
+            if(ciphertext.charCodeAt(i) >= 97 && ciphertext.charCodeAt(i) <= 122){
+                x += 97;
+            }
+
+            plaintext += String.fromCharCode(x);
+
         }
 
-        if(ciphertext.charCodeAt(i) >= 65 && ciphertext.charCodeAt(i) <= 90){
-            tempciphertext = ciphertext.charCodeAt(i) - 65;
+        document.getElementById("hasil").innerHTML = plaintext;
+    } else{
+        var key = "";
+        for(let i = 0;i<ciphertext.length; i++){
+            key += keyLess[i];
         }
 
-        if(ciphertext.charCodeAt(i) >= 97 && ciphertext.charCodeAt(i) <= 122){
-            tempciphertext = ciphertext.charCodeAt(i) - 97;
+        for(let i = 0 ; i < ciphertext.length ; i++ ){
+            if(ciphertext[i] == ' '){
+                plaintext += ' ';
+                conkey += 1;
+                continue;
+            }
+
+            if(ciphertext.charCodeAt(i) >= 65 && ciphertext.charCodeAt(i) <= 90){
+                tempciphertext = ciphertext.charCodeAt(i) - 65;
+            }
+
+            if(ciphertext.charCodeAt(i) >= 97 && ciphertext.charCodeAt(i) <= 122){
+                tempciphertext = ciphertext.charCodeAt(i) - 97;
+            }
+
+            if(key.charCodeAt(i-conkey) >= 65 && key.charCodeAt(i) <= 90){
+                tempkey = key.charCodeAt(i-conkey) - 65;
+            }
+
+            if(key.charCodeAt(i-conkey) >= 97 && key.charCodeAt(i) <= 122){
+                tempkey = key.charCodeAt(i-conkey) - 97;
+            }
+
+            x = ma_mod((tempciphertext - tempkey),26);
+
+            if(ciphertext.charCodeAt(i) >= 65 && ciphertext.charCodeAt(i) <= 90){
+                x += 65;
+            }
+
+            if(ciphertext.charCodeAt(i) >= 97 && ciphertext.charCodeAt(i) <= 122){
+                x += 97;
+            }
+
+            plaintext += String.fromCharCode(x);
+
         }
 
-        if(key.charCodeAt(i-conkey) >= 65 && key.charCodeAt(i) <= 90){
-            tempkey = key.charCodeAt(i-conkey) - 65;
-        }
-
-        if(key.charCodeAt(i-conkey) >= 97 && key.charCodeAt(i) <= 122){
-            tempkey = key.charCodeAt(i-conkey) - 97;
-        }
-
-        x = ma_mod((tempciphertext - tempkey),26);
-
-        if(ciphertext.charCodeAt(i) >= 65 && ciphertext.charCodeAt(i) <= 90){
-            x += 65;
-        }
-
-        if(ciphertext.charCodeAt(i) >= 97 && ciphertext.charCodeAt(i) <= 122){
-            x += 97;
-        }
-
-        plaintext += String.fromCharCode(x);
+        document.getElementById("hasil").innerHTML = plaintext;
 
     }
-
-    document.getElementById("hasil").innerHTML = plaintext;
 }
 
 
